6bafe4412f9c34e8147cc47a51978f32559ad5af
garvitkat <garvitvanshkataria@gmail.com>
2019-10-20 17:43:57 +0530

    LiveView
    live clothes live

---
+++ Assets/scr.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class scr : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbButtonObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbButtonObject = GameObject.Find("one");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Application.OpenURL("file:///C:/Users/Lenovo/Desktop/essence/index.html");
+        Debug.Log("Website Button Down");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Website Button released");
+    }
+
+}
+
